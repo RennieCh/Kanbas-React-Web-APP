4d0d02fdc3992448a9fc3bcb32c2f3ba8fe02f93
RennieCh <119545126+RennieCh@users.noreply.github.com>
2024-09-15 21:56:07 -0400

    kanbas a1 v4 added githun

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,10 @@ export default function TOC() {
            <li><Link to="/Labs/Lab2">Lab 2</Link></li>
            <li><Link to="/Labs/Lab3">Lab 3</Link></li>
            <li><Link to="/Kanbas">Kanbas</Link></li>
+           <li><a href="https://github.com/RennieCh/kanbas-react-web-app-cs5610-fa24" 
+           id="wd-github" 
+           target="_blank" 
+           rel="noopener noreferrer">GitHub</a></li>
         </ul>
     )
 }
