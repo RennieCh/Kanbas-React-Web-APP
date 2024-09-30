916bf389af491f10a45f85398637f993c20e4f4c
RennieCh <119545126+RennieCh@users.noreply.github.com>
2024-09-29 22:42:50 -0400

    kanbas add people

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -1,15 +1,13 @@
 import Modules from "../Modules";
 import CourseStatus from "./Status";
 export default function Home() {
-    return(
-        <div id="wd-home" className="container">
-            <div className="row">
-                <div className="col-12 col-lg-10">
-                    <Modules />
-                </div>
-                <div className="col-2 d-none d-lg-block float-left">
-                    <CourseStatus />
-                </div>
+    return (
+        <div id="wd-home" className="d-flex">
+            <div className="flex-fill">
+                <Modules />
+            </div>
+            <div className="d-none d-md-block">
+                <CourseStatus />
             </div>
         </div>
     )
