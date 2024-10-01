231288271d07e1f631d00a41172be6c8812247a9
RennieCh <119545126+RennieCh@users.noreply.github.com>
2024-09-30 22:44:36 -0400

    kanbas add profile

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -1,21 +1,79 @@
 import { Link } from "react-router-dom";
+
 export default function Profile() {
   return (
-    <div id="wd-profile-screen">
+    <div id="wd-profile-screen" className="p-3">
       <h3>Profile</h3>
-      <input id="wd-username" value="alice" placeholder="username" /><br/>
-      <input id="wd-password" value="123" placeholder="password"
-             type="password" /><br/>
-      <input id="wd-firstname" value="Alice" placeholder="First Name" /><br/>
-      <input id="wd-lastname" value="Wonderland" placeholder="Last Name" /><br/>
-      <input id="wd-dob" value="2000-01-01" type="date" /><br/>
-      <input id="wd-email" value="alice@wonderland" type="email" /><br/>
-      <select id="wd-role">
-        <option value="USER">User</option>
-        <option value="ADMIN">Admin</option>
-        <option value="FACULTY">Faculty</option>
-        <option value="STUDENT">Student</option>
-      </select><br/>
-      <Link to="/Kanbas/Account/Signin" >Sign out</Link>
+      <div className="mb-3">
+        <input
+          type="text"
+          className="form-control"
+          id="wd-username"
+          placeholder="alice"
+          value="alice"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          id="wd-password"
+          className="form-control"
+          placeholder="123"
+          value="123"
+          type="password"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          id="wd-firstname"
+          className="form-control"
+          placeholder="Alice"
+          value="Alice"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          id="wd-lastname"
+          className="form-control"
+          placeholder="Wonderland"
+          value="Wonderland"
+        />
+      </div>
+
+      <div className="mb-3 input-group">
+        <input
+          id="wd-dob"
+          className="form-control"
+          placeholder="mm/dd/yyyy"
+          type="date"
+          value="2024-05-16"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          id="wd-email"
+          className="form-control"
+          placeholder="alice@wonderland.com"
+          value="alice@wonderland.com"
+          type="email"
+        />
+      </div>
+
+      <div className="mb-4">
+        <select id="wd-role" className="form-select">
+          <option value="USER">User</option>
+          <option value="ADMIN">Admin</option>
+          <option value="FACULTY">Faculty</option>
+          <option value="STUDENT">Student</option>
+        </select>
+      </div>
+
+      <Link to="/Kanbas/Account/Signin" className="btn btn-danger w-100 text-center">
+        Signout
+      </Link>
     </div>
-);}
+  );
+}
