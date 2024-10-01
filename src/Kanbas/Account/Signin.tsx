f512807b7167389ccb367cbdfa5a18e3c77f3e70
RennieCh <119545126+RennieCh@users.noreply.github.com>
2024-09-30 22:02:44 -0400

    Kanbas add signin & signup

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -1,12 +1,39 @@
 import { Link } from "react-router-dom";
+
 export default function Signin() {
   return (
-    <div id="wd-signin-screen">
-      <h3>Sign in</h3>
-      <input id="wd-username" placeholder="username" /> <br />
-      <input id="wd-password" placeholder="password" type="password" /> <br />
-      <Link  id="wd-signin-btn"
-             to="/Kanbas/Dashboard"> Sign in </Link> <br />
-      <Link  id="wd-signup-link" to="/Kanbas/Account/Signup">Sign up</Link>
-</div>
-);}
+    <div id="wd-signin-screen" className="container d-flex flex-column mt-5">
+      <h3 className="mb-3">Signin</h3>
+
+      <input
+        id="wd-username"
+        className="form-control mb-2"
+        placeholder="username"
+        type="text"
+      />
+
+      <input
+        id="wd-password"
+        className="form-control mb-2"
+        placeholder="password"
+        type="password"
+      />
+
+      <Link
+        id="wd-signin-btn"
+        to="/Kanbas/Dashboard"
+        className="btn btn-primary align-items-center w-100 mb-2"
+      >
+        Signin
+      </Link>
+
+      <Link
+        id="wd-signup-link"
+        to="/Kanbas/Account/Signup"
+        className="text-left"
+      >
+        Signup
+      </Link>
+    </div>
+  );
+}
--- src/Kanbas/Account/Signup.tsx
+++ src/Kanbas/Account/Signup.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+
 import { Link } from "react-router-dom";
 export default function Signup() {
   return (
-    <div id="wd-signup-screen">
-      <h3>Sign up</h3>
-      <input placeholder="username" /><br/>
-      <input placeholder="password" type="password" /><br/>
-      <input placeholder="verify password" type="password" /><br/>
-      <Link to="/Kanbas/Account/Profile" > Sign up </Link><br />
+    <div id="wd-signup-screen" className="container-fluid d-flex flex-column mt-4">
+      <h3 className="mb-3">Signup</h3>
+      <input
+        id="wd-username"
+        className="form-control mb-2"
+        placeholder="username"
+        type="text"
+      />
+
+      <input
+        id="wd-password"
+        className="form-control mb-2"
+        placeholder="password"
+        type="password"
+      />
+
+      <input
+        id="wd-password"
+        className="form-control mb-2"
+        placeholder="verify password"
+        type="password"
+      />
+
+      <Link to="/Kanbas/Account/Profile"
+        className="btn btn-primary align-items-center w-100 mb-2"> 
+        Signup </Link>
+
       <Link to="/Kanbas/Account/Signin" >Sign in</Link>
-</div>
-);}
+    </div>
+  );
+}
