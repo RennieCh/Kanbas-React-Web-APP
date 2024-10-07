5e8139e14c6586fe7c9dd1ae4a7684cec0d7ea7b
RennieCh <119545126+RennieCh@users.noreply.github.com>
2024-10-07 00:46:08 -0400

    kanbas 3.6 course navigation

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,26 +1,27 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useLocation } from "react-router-dom";
 
 export default function CoursesNavigation() {
   const { cid } = useParams(); // Get course ID from the URL
+  const { pathname } = useLocation(); // Get current pathname from the URL
+
+  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades"];
+
+  // Helper function to check if a link is active
+  const isActiveLink = (link: string): boolean => {
+    return pathname.includes(link); // Check if the current pathname contains the link name
+  };
 
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0 d-none d-lg-block">
-      <Link id="wd-course-home-link" to={`/Kanbas/Courses/${cid}/Home`} 
-      className="list-group-item active border border-0">Home</Link>
-      <Link id="wd-course-modules-link" to={`/Kanbas/Courses/${cid}/Modules`} 
-      className="list-group-item text-danger border border-0">Modules</Link>
-      <Link id="wd-course-piazza-link" to={`/Kanbas/Courses/${cid}/Piazza`} 
-      className="list-group-item text-danger border border-0">Piazza</Link>
-      <Link id="wd-course-zoom-link" to={`/Kanbas/Courses/${cid}/Zoom`} 
-      className="list-group-item text-danger border border-0">Zoom</Link>
-      <Link id="wd-course-quizzes-link" to={`/Kanbas/Courses/${cid}/Assignments`} 
-      className="list-group-item text-danger border border-0">Assignments</Link>
-      <Link id="wd-course-assignments-link" to={`/Kanbas/Courses/${cid}/Quizzes`} 
-      className="list-group-item text-danger border border-0">Quizzes</Link>
-      <Link id="wd-course-grades-link" to={`/Kanbas/Courses/${cid}/Grades`} 
-      className="list-group-item text-danger border border-0">Grades</Link>
-      <Link id="wd-course-people-link" to={`/Kanbas/Courses/${cid}/People`} 
-      className="list-group-item text-danger border border-0">People</Link>
+      {links.map((link) => (
+        <Link
+          key={link}
+          to={`/Kanbas/Courses/${cid}/${link}`}
+          className={`list-group-item border border-0 ${isActiveLink(link) ? "active" : "text-danger"}`}
+        >
+          {link}
+        </Link>
+      ))}
     </div>
   );
 }
