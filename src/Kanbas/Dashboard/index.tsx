9acfd49e27a49338436d74a155f34fd28c50dc85
RennieCh <119545126+RennieCh@users.noreply.github.com>
2024-10-23 01:34:46 -0400

    Kanbas 3.1.1

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,17 +1,49 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as db from "../Database";
 
 export default function Dashboard() {
-    const courses = db.courses;
+    // Convert courses constant to a state variable
+    const [courses, setCourses] = useState(db.courses);
+
+    const [course, setCourse] = useState<any>({
+        _id: "0", name: "New Course", number: "New Number",
+        startDate: "2023-09-10", endDate: "2023-12-15",
+        image: "/images/reactjs.jpg", description: "New Description"
+    });
 
     // Function to generate the image path based on course _id
     const getImagePath = (courseId: string): string => {
-        return `/images/${courseId.toLowerCase()}.jpg`;
+        // If the courseId starts with "RS", return the corresponding image
+        if (courseId.startsWith("RS")) {
+            return `/images/${courseId.toLowerCase()}.jpg`;
+        }
+        // Otherwise, use the default reactjs image
+        return "/images/reactjs.jpg";
+    };
+
+    // create addNewCourse event handler that sets courses as copy of current courses state array
+    // add course at the end of the array, overriding _id to current time stamp
+    const addNewCourse = () => {
+        const newCourse = {
+            ...course,
+            _id: new Date().getTime().toString()
+        };
+        setCourses([...courses, { ...course, ...newCourse }]);
     };
 
     return (
         <div id="wd-dashboard">
             <h1 id="wd-dashboard-title">Dashboard</h1>
+            <h5>New Course
+                <button className="btn btn-primary float-end"
+                    id="wd-add-new-course-click"
+                    onClick={addNewCourse} > Add
+                </button> </h5><br />
+            <input defaultValue={course.name} className="form-control mb-2"
+                onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+            <textarea defaultValue={course.description} className="form-control"
+                onChange={(e) => setCourse({ ...course, description: e.target.value })} />
             <hr />
             <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
             <hr />
