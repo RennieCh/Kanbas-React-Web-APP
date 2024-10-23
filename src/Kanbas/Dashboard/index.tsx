678173f0b54f33296caf0dc5e1864a6bfeae5ff6
RennieCh <119545126+RennieCh@users.noreply.github.com>
2024-10-23 11:23:59 -0400

    Kanbas 3.1.3 Editing a Course

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import * as db from "../Database";
 
 export default function Dashboard() {
-    // Convert courses constant to a state variable
     const [courses, setCourses] = useState(db.courses);
 
     const [course, setCourse] = useState<any>({
@@ -14,27 +13,32 @@ export default function Dashboard() {
 
     // Function to generate the image path based on course _id
     const getImagePath = (courseId: string): string => {
-        // If the courseId starts with "RS", return the corresponding image
         if (courseId.startsWith("RS")) {
             return `/images/${courseId.toLowerCase()}.jpg`;
         }
-        // Otherwise, use the default reactjs image
         return "/images/reactjs.jpg";
     };
 
-    // create addNewCourse event handler that sets courses as copy of current courses state array
-    // add course at the end of the array, overriding _id to current time stamp
     const addNewCourse = () => {
         const newCourse = {
             ...course,
             _id: new Date().getTime().toString()
         };
-        setCourses([...courses, { ...course, ...newCourse }]);
+        setCourses([...courses, newCourse]);
     };
 
-    // add deleteCourse event handler accepting ID of course to remove by filtering out the course by its ID
     const deleteCourse = (courseId: string) => {
         setCourses(courses.filter((course) => course._id !== courseId));
+    };
+
+    const updateCourse = () => {
+        setCourses(courses.map((c: any) => {
+            if (c._id === course._id) {
+                return course;
+            } else {
+                return c;
+            }
+        }));
     };
 
     return (
@@ -42,13 +46,24 @@ export default function Dashboard() {
             <h1 id="wd-dashboard-title">Dashboard</h1>
             <h5>New Course
                 <button className="btn btn-primary float-end"
-                    id="wd-add-new-course-click"
-                    onClick={addNewCourse} > Add
-                </button> </h5><br />
-            <input defaultValue={course.name} className="form-control mb-2"
-                onChange={(e) => setCourse({ ...course, name: e.target.value })} />
-            <textarea defaultValue={course.description} className="form-control"
-                onChange={(e) => setCourse({ ...course, description: e.target.value })} />
+                    id="wd-add-new-course-click" onClick={addNewCourse}>
+                    Add
+                </button>
+                <button className="btn btn-warning float-end me-2"
+                    onClick={updateCourse} id="wd-update-course-click">
+                    Update
+                </button>
+            </h5><br />
+            <input
+                value={course.name}
+                className="form-control mb-2"
+                onChange={(e) => setCourse({ ...course, name: e.target.value })}
+            />
+            <textarea
+                value={course.description}
+                className="form-control"
+                onChange={(e) => setCourse({ ...course, description: e.target.value })}
+            />
             <hr />
             <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
             <hr />
@@ -66,20 +81,21 @@ export default function Dashboard() {
                                     />
                                     <div className="card-body">
                                         <h5 className="wd-dashboard-course-title card-title"
-                                            style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}
-                                        >
+                                            style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
                                             {course.name}
                                         </h5>
                                         <p className="wd-dashboard-course-title card-text" style={{ maxHeight: 100, overflow: "hidden" }}>
                                             {course.description}
                                         </p>
                                         <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">Go</Link>
-                                        <button onClick={(event) => {
-                                            event.preventDefault();
-                                            deleteCourse(course._id);
-                                        }} className="btn btn-danger float-end"
-                                            id="wd-delete-course-click">Delete</button>
-
+                                        <button onClick={(event) => { event.preventDefault(); deleteCourse(course._id); }}
+                                            className="btn btn-danger float-end" id="wd-delete-course-click">
+                                            Delete
+                                        </button>
+                                        <button id="wd-edit-course-click" onClick={(event) => { event.preventDefault(); setCourse(course); }}
+                                            className="btn btn-warning me-2 float-end">
+                                            Edit
+                                        </button>
                                     </div>
                                 </div>
                             </Link>
