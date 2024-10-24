4484683168978ebc4da6b8dc743c225162dce1cb
RennieCh <119545126+RennieCh@users.noreply.github.com>
2024-10-24 17:56:51 -0400

    Kanbas 3.5.4 role-access to Editor

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -5,7 +5,7 @@ import { VscNotebook } from "react-icons/vsc";
 import AssignmentControlButtons from "./AssignmentControlButtons";
 import LessonControlButtons from "../Modules/LessonControlButtons";
 import { FaTrash } from "react-icons/fa";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { format } from 'date-fns'; // To format the dates
 import { useSelector, useDispatch } from "react-redux";
 import { deleteAssignment } from "./reducer";
@@ -16,7 +16,9 @@ import { Modal, Button } from 'react-bootstrap';
 export default function Assignments() {
     const { cid } = useParams(); // Get the course ID from the URL
     const { assignments } = useSelector((state: any) => state.assignmentsReducer); // Use assignments from the store
+    const { currentUser } = useSelector((state: any) => state.accountReducer); // Get currentUser from accountReducer
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const [showModal, setShowModal] = useState(false);
     const [selectedAssignment, setSelectedAssignment] = useState<any>(null);
@@ -58,10 +60,20 @@ export default function Assignments() {
         setShowModal(false); // Just close the modal
     };
 
+    // Handle assignment click
+    const handleAssignmentClick = (assignmentId: string) => {
+        if (currentUser.role === "FACULTY") {
+            navigate(`/Kanbas/Courses/${cid}/Assignments/${assignmentId}`);
+        } else {
+            // Prevent non-FACULTY users from navigating to the AssignmentEditor
+            alert("You do not have permission to edit this assignment.");
+        }
+    };
+
     return (
         <div className="container-fluid">
             <div id="wd-assignments">
-                <AssignmentsControls />
+                {currentUser.role === "FACULTY" && <AssignmentsControls />}
                 <br /><br />
 
                 <ul id="wd-assignment-list" className="list-group rounded-0 w-100">
@@ -84,22 +96,26 @@ export default function Assignments() {
                                         </div>
 
                                         <div className="text-left flex-grow-1">
-                                            <Link className="wd-assignment-link"
-                                                to={`/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}
-                                                style={{ textDecoration: "none", fontWeight: "bold", color: "black" }}>
+                                            <span
+                                                className="wd-assignment-link"
+                                                onClick={() => handleAssignmentClick(assignment._id)}
+                                                style={{ cursor: "pointer", fontWeight: "bold", color: "black", textDecoration: "none" }}
+                                            >
                                                 {assignment.title}
-                                            </Link>
+                                            </span>
                                             <br />
                                             <span className="text-danger">Multiple Modules</span> |
                                             <b>Not available until</b> {formatDateTime(assignment.available_date)} |<br />
                                             <b>Due</b> {formatDateTime(assignment.due_date, true)} | {assignment.points} pts
                                         </div>
-                                        {/* Delete Icon */}
-                                        <FaTrash
-                                            className="text-danger me-2 mb-1"
-                                            style={{ cursor: "pointer" }}
-                                            onClick={() => handleDeleteClick(assignment)}
-                                        />
+                                        {/* Only show delete icon if the user is FACULTY */}
+                                        {currentUser.role === "FACULTY" && (
+                                            <FaTrash
+                                                className="text-danger me-2 mb-1"
+                                                style={{ cursor: "pointer" }}
+                                                onClick={() => handleDeleteClick(assignment)}
+                                            />
+                                        )}
                                         <LessonControlButtons />
                                     </div>
                                 </li>
