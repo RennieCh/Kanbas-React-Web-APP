093ee428ff1b7a872a632b574fb4dcb8e28c25a0
RennieCh <119545126+RennieCh@users.noreply.github.com>
2024-11-04 01:02:27 -0500

    a5 lab5 3.5.4

--- src/Labs/Lab5/HttpClient.tsx
+++ src/Labs/Lab5/HttpClient.tsx
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as client from "./client";
 
 export default function HttpClient() {
     const [welcomeOnClick, setWelcomeOnClick] = useState("");
+    const [welcomeOnLoad, setWelcomeOnLoad] = useState("");
+
     const fetchWelcomeOnClick = async () => {
         const message = await client.fetchWelcomeMessage();
         setWelcomeOnClick(message);
     };
+
+    const fetchWelcomeOnLoad = async () => {
+        const welcome = await client.fetchWelcomeMessage();
+        setWelcomeOnLoad(welcome);
+    };
+    useEffect(() => {
+        fetchWelcomeOnLoad();
+    }, []);
+
     return (
         <div>
             <h3>HTTP Client</h3> <hr />
@@ -15,6 +26,10 @@ export default function HttpClient() {
                 Fetch Welcome
             </button> <br />
             Response from server: <b>{welcomeOnClick}</b>
+            <hr />
+            <h4>Requesting on Load</h4>
+            Response from server: <b>{welcomeOnLoad}</b>
+            <hr />
         </div>
     );
 }
