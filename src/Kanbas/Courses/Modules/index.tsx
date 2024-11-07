b839710456005bad47226414c0dc28c6eca47305
RennieCh <119545126+RennieCh@users.noreply.github.com>
2024-11-07 01:53:58 -0500

    a5 kanbas 4.5.3

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -7,6 +7,7 @@ import { useState, useEffect, useCallback } from "react";
 import { setModules, addModule, editModule, updateModule, deleteModule } from "./reducer";
 import { useSelector, useDispatch } from "react-redux";
 import * as coursesClient from "../client";
+import * as modulesClient from "./client";
 
 export default function Modules() {
   const { cid } = useParams(); // Get the course ID from the URL
@@ -20,6 +21,11 @@ export default function Modules() {
     const newModule = { name: moduleName, course: cid };
     const module = await coursesClient.createModuleForCourse(cid, newModule);
     dispatch(addModule(module));
+  };
+
+  const removeModule = async (moduleId: string) => {
+    await modulesClient.deleteModule(moduleId);
+    dispatch(deleteModule(moduleId));
   };
 
   const fetchModules = useCallback(async () => {
@@ -63,7 +69,7 @@ export default function Modules() {
               )}
               {currentUser.role === "FACULTY" && (
                 <ModuleControlButtons moduleId={module._id}
-                  deleteModule={(moduleID) => { dispatch(deleteModule(moduleID)); }}
+                  deleteModule={(moduleId) => removeModule(moduleId)}
                   editModule={(moduleId) => dispatch(editModule(moduleId))} />
               )}
             </div>
